6c882eed2ab0da8e5fe224f7c73dc088c7687a69
umsungjun <umseongjun@naver.com>
2024-08-27 21:36:49 +0900

    Tailwind Css responsive 연습

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2">
+    <main className="bg-gray-100  sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-yellow-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 md:flex-row">
         <input
           className="w-full rounded-full h-12 bg-gray-200 px-5 outline-none ring ring-transparent focus:ring-orange-500 transition-shadow"
           type="text"
@@ -9,7 +9,7 @@ export default function Home() {
         />
         <button
           className="bg-orange-500  text-white
-        py-3 rounded-full active:scale-90 transition-transform font-medium focus:outline-none"
+        py-3 rounded-full active:scale-90 transition-transform font-medium focus:outline-none md:px-5"
         >
           Search
         </button>
