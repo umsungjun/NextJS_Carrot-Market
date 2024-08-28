a737aff1b87f11687d6b5a38e7b99e54f140c7db
umsungjun <umseongjun@naver.com>
2024-08-28 21:15:21 +0900

    Tailwind Css has 연습

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className="bg-gray-100  sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-yellow-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 md:flex-row">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 md:flex-row *:outline-none has-[input]:text-red-500">
         <input
-          className="w-full rounded-full h-12 bg-gray-200 px-5 outline-none ring ring-transparent focus:ring-orange-500 transition-shadow invalid:focus:ring-red-500 peer"
+          className="w-full rounded-full h-12 bg-gray-200 px-5  ring ring-transparent focus:ring-orange-500 transition-shadow peer"
           type="email"
           required
           placeholder="Search here..."
@@ -13,7 +13,7 @@ export default function Home() {
         </span>
         <button
           className="bg-orange-500  text-white
-        py-3 rounded-full active:scale-90 transition-transform font-medium focus:outline-none md:px-5"
+        py-3 rounded-full active:scale-90 transition-transform font-medium md:px-5"
         >
           Search
         </button>
