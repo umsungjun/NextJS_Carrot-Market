dca2185ebb3d7a7d4fc794b96a8e0e9e2cb64eb2
umsungjun <umseongjun@naver.com>
2024-09-04 21:46:03 +0900

    Feat: form-input errors props 필수값 아님 처리

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -3,7 +3,7 @@ interface FormInputProps {
   type: string;
   placeholder: string;
   required: boolean;
-  errors: string[];
+  errors?: string[];
 }
 
 export default function FormInput({
@@ -11,7 +11,7 @@ export default function FormInput({
   type,
   placeholder,
   required,
-  errors,
+  errors = [],
 }: FormInputProps) {
   return (
     <div className="flex flex-col gap-2">
