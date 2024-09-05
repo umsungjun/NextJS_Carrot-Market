dd28115f07d82abc2b1bcde3aa194a6a31bb3a75
umsungjun <umseongjun@naver.com>
2024-09-05 21:36:04 +0900

    Feat: create-account Form zod validation message custom 처리

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,12 +1,35 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(5),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirmPassword: z.string().min(10),
-});
+const checkUserName = (userName: string) => !userName.includes("fuck");
+const checkPassword = ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => password === confirmPassword;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string!",
+        required_error: "Where is my userName?",
+      })
+      .min(3, "Way too short!!!")
+      .max(5, "That is too long!!!")
+      /* refine: 특정 단어 validation / false를 반환하면 에러문구 반환 */
+      .refine(checkUserName, "no Fuck"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirmPassword: z.string().min(10),
+  })
+  /* 전체 refine이기 때문에 어떤 filed에서 일어난 에러인지 명확하게 표시 해야됨 */
+  .refine(checkPassword, {
+    message: "Both passwords should be the same!",
+    path: ["confirmPassword"],
+  });
 
 export const createAccount = async (prevState: any, formData: FormData) => {
   const data = {
@@ -19,6 +42,7 @@ export const createAccount = async (prevState: any, formData: FormData) => {
 
   if (!result.success) {
     /* flatten() key:["error"] return 해줌 */
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 };
