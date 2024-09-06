08a5192a5903429c58bdc02a31358effad5c0b15
umsungjun <umseongjun@naver.com>
2024-09-06 21:25:58 +0900

    Rename: form-input.tsx -> Input.tsx 변경

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,26 +1,17 @@
-interface FormInputProps {
+import { InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  type: string;
-  placeholder: string;
-  required: boolean;
   errors?: string[];
 }
 
-export default function FormInput({
-  name,
-  type,
-  placeholder,
-  required,
-  errors = [],
-}: FormInputProps) {
+export default function Input({ name, errors = [], ...rest }: InputProps) {
   return (
     <div className="flex flex-col gap-2">
       <input
         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
         name={name}
-        type={type}
-        placeholder={placeholder}
-        required={required}
+        {...rest}
       />
       {errors.map((error, index) => {
         return (
