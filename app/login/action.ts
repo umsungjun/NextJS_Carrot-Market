0981dd9d581390d54846c7836dd8abf249058d34
umsungjun <umseongjun@naver.com>
2024-10-10 21:24:26 +0900

    Feat: 이메일 로그인 구현

--- app/login/action.ts
+++ app/login/action.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { redirect } from "next/navigation";
+import bcrypt from "bcrypt";
+
 import {
   PASSWORD_MAX_ERROR,
   PASSWORD_MAX_LENGTH,
@@ -8,10 +11,29 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import { db } from "@/lib/db";
 import { z } from "zod";
+import { getSession } from "@/lib/session";
+
+/* 입력한 이메일을 사용하는 유저 존재 여부 */
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExists, "입력하신 이메일 주소가 존재하지 않습니다."),
   password: z
     .string({
       required_error: "비밀번호는 필수 값입니다.",
@@ -27,11 +49,39 @@ export const login = async (prevState: any, formData: FormData) => {
     password: formData.get("password"),
   };
 
-  const result = formSchema.safeParse(data);
+  /* refines를 쓰는 검증이 있다면  safeParse -> safeParseAsync로 변경 해야됨*/
+  const result = await formSchema.safeParseAsync(data);
 
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result);
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+    /* compare 앞에 인자(비밀번호)와 뒤에 인자(해쉬 된 비밀번호)가 같은지 비교 */
+    const ok = await bcrypt.compare(result.data.password, user!.password ?? "");
+
+    if (ok) {
+      const session = await getSession();
+
+      session.id = user!.id;
+
+      await session.save();
+
+      redirect("/profile");
+    } else {
+      return {
+        fieldErrors: {
+          email: [],
+          password: ["잘못된 비밀번호입니다. 한 번 더 확인해주세요."],
+        },
+      };
+    }
   }
 };
