2b179deafe3795abb098a26735c1eb5654013c36
umsungjun <umseongjun@naver.com>
2024-10-21 21:32:03 +0900

    Feat: middleware를 통해 로그인 상태에 따른 block 처리

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+/* 로그인 하지 않았을 경우 접속 가능 페이지 */
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+  "/github/start": true,
+  "/github/complete": true,
+};
 
 /* 
   middleware Trigger
   페이지를 바꿀 떄
   브라우저가 css, javascript, favicon 파일을 다운 받을 때
-*/
 
+  node.js runtime에 실행되지 않음, Edge runtime이라는 곳에서 실행 됨
+*/
 export const middleware = async (request: NextRequest) => {
-  console.log("hello test");
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+
+  if (!session.id) {
+    if (!exists) {
+      /* 로그인하지 않고 publicOnlyUrls 페이지 접속 시 이전 페이지로 redirect */
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
+  }
 };
 
 export const config = {
