eaccd42a7ad48d1b7ccc82b13ed67d8d39a181ea
umsungjun <umseongjun@naver.com>
2024-10-28 15:26:53 +0900

    Feat: gitHub 로그인 시 유저 존재 여부에 따른 분기 처리 완료

--- app/github/complete/route.ts
+++ app/github/complete/route.ts
@@ -1,4 +1,6 @@
-import { notFound } from "next/navigation";
+import { db } from "@/lib/db";
+import { getSession } from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -8,6 +10,7 @@ export async function GET(request: NextRequest) {
     return notFound();
   }
 
+  /* accessToken 받는 로직 */
   const accessTokenParam = new URLSearchParams({
     client_id: process.env.GITHUB_CLIENT_ID!,
     client_secret: process.env.GITHUB_CLIENT_SECRET!,
@@ -21,13 +24,55 @@ export async function GET(request: NextRequest) {
       Accept: " application/json",
     },
   });
-  const accessTokenData = await accessTokenResponse.json();
+  const { error, access_token } = await accessTokenResponse.json();
 
-  if ("error" in accessTokenData) {
+  if (error) {
     return new Response(null, {
       status: 400,
     });
   }
 
-  return Response.json({ accessTokenData });
+  const userProfileResponse = await fetch("https://api.github.com/user", {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+    /* 기본적으로 GET request들은 Next.js의 cache에 의해서 저장 됨 */
+    cache: "no-cache",
+  });
+  /* login = gitHub userName */
+  const { id, avatar_url, login } = await userProfileResponse.json();
+
+  /* 사용자 존재 여부 */
+  const user = await db.user.findUnique({
+    where: {
+      github_id: id + "",
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (user) {
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
+    return redirect("/profile");
+  }
+
+  const newUser = await db.user.create({
+    data: {
+      github_id: id + "",
+      avatar: avatar_url,
+      /* ToDo GitHub계정의 닉네임이 중복 된 닉네임일 수 있음 */
+      username: login,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  const session = await getSession();
+  session.id = newUser.id;
+  await session.save();
+  return redirect("/profile");
 }
