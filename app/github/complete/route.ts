2c92ff23180757b0375ede46300640b21f8f72a4
umsungjun <umseongjun@naver.com>
2024-10-30 21:39:45 +0900

    Refactor: github 계정으로 로그인 시 관련 fetch로직 분리

--- app/github/complete/route.ts
+++ app/github/complete/route.ts
@@ -1,6 +1,6 @@
-import { db } from "@/lib/db";
-import { getSession, saveSession } from "@/lib/session";
-import { redirect } from "next/navigation";
+import { db, findUserByGithubId } from "@/lib/db";
+import { getAccessToken, getUserProfile } from "@/lib/github";
+import { saveSession } from "@/lib/session";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -12,21 +12,8 @@ export async function GET(request: NextRequest) {
     });
   }
 
-  /* accessToken 받는 로직 */
-  const accessTokenParam = new URLSearchParams({
-    client_id: process.env.GITHUB_CLIENT_ID!,
-    client_secret: process.env.GITHUB_CLIENT_SECRET!,
-    code,
-  }).toString();
-  const accessTokenURL = `https://github.com/login/oauth/access_token?${accessTokenParam}`;
-
-  const accessTokenResponse = await fetch(accessTokenURL, {
-    method: "POST",
-    headers: {
-      Accept: " application/json",
-    },
-  });
-  const { error, access_token } = await accessTokenResponse.json();
+  /* 1. access_token 받아오기 */
+  const { error, access_token } = await getAccessToken(code);
 
   if (error) {
     return new Response(null, {
@@ -34,44 +21,27 @@ export async function GET(request: NextRequest) {
     });
   }
 
-  const userProfileResponse = await fetch("https://api.github.com/user", {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-    /* 기본적으로 GET request들은 Next.js의 cache에 의해서 저장 됨 */
-    cache: "no-cache",
-  });
-  /* login = gitHub userName */
-  const { id, avatar_url, login } = await userProfileResponse.json();
+  /* 2. github 유저 정보 받아오기 */
+  const { id, avatar_url, login } = await getUserProfile(access_token);
 
-  /* 사용자 존재 여부 */
-  const user = await db.user.findUnique({
-    where: {
-      github_id: id + "",
-    },
-    select: {
-      id: true,
-    },
-  });
+  /* 3. github_id로 유저 존재 여부 확인 */
+  const user = await findUserByGithubId(String(id));
 
+  /* 기존 회원일 때 세션에 유저 아이디 저장 후 profile 페이지로 redirect */
   if (user) {
     await saveSession(user.id);
+  } else {
+    const newUser = await db.user.create({
+      data: {
+        github_id: id + "",
+        avatar: avatar_url,
+        /* ToDo GitHub계정의 닉네임이 중복 된 닉네임일 수 있음 */
+        username: login,
+      },
+      select: {
+        id: true,
+      },
+    });
+    await saveSession(newUser.id);
   }
-
-  const newUser = await db.user.create({
-    data: {
-      github_id: id + "",
-      avatar: avatar_url,
-      /* ToDo GitHub계정의 닉네임이 중복 된 닉네임일 수 있음 */
-      username: login,
-    },
-    select: {
-      id: true,
-    },
-  });
-
-  const session = await getSession();
-  session.id = newUser.id;
-  await session.save();
-  return redirect("/profile");
 }
