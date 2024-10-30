f8df50482e2645f24c8e294b2f401d3bfcdc0fd2
umsungjun <umseongjun@naver.com>
2024-10-30 21:39:14 +0900

    Feat: github_id로 유저 탐색 함수 분리

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 
 export const db = new PrismaClient();
+
+/* github_id로 유저 탐색 함수 */
+export const findUserByGithubId = async (github_id: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      github_id,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return user;
+};
