4bc9dcd1d79e9637b19e27090ed47b2593e7a181
umsungjun <umseongjun@naver.com>
2024-11-25 21:42:43 +0900

    Feat: products 로딩 컴포넌트 생성

--- app/(tabs)/products/loading.tsx
+++ app/(tabs)/products/loading.tsx
@@ -5,10 +5,9 @@ export default function Loading() {
         <div key={index} className="*:rounded-md flex gap-5">
           <div className="min-w-28 min-h-28 bg-neutral-700" />
           <div className="flex flex-col gap-2 w-full">
-            <div className="bg-neutral-700 h-5 w-4/5"></div>
-            <div className="bg-neutral-700 h-5 w-3/5"></div>
-            <div className="bg-neutral-700 h-5 w-2/5"></div>
-            <div className="bg-neutral-700 h-5 w-1/5"></div>
+            <div className="bg-neutral-700 h-5 w-5/6"></div>
+            <div className="bg-neutral-700 h-5 w-3/6"></div>
+            <div className="bg-neutral-700 h-5 w-1/6"></div>
           </div>
         </div>
       ))}
