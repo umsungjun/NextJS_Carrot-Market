93d88dd17fdfe5ea1632b7676d35571fcf8ea9a9
umsungjun <umseongjun@naver.com>
2024-12-08 20:58:11 +0900

    Feat: 무한 스크롤 테스트
    - Next.js에 내장되어있는 IntersectionObserver를 활용한 테스트

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ListProduct from "./list-product";
 import { getMoreProducts } from "@/app/(tabs)/products/action";
 
@@ -21,37 +21,59 @@ export default function ProductList({ initialProducts }: ProductListProps) {
   const [page, setPage] = useState(0);
   /* 마지막 페이지 여부 */
   const [isLastPage, setIsLastPage] = useState(false);
+  const trigger = useRef<HTMLSpanElement>(null);
 
-  const onLoadMoreClick = async () => {
-    setIsLoading(true);
-    const newProducts = await getMoreProducts(page + 1);
-
-    if (newProducts.length !== 0) {
-      setPage((prev) => prev + 1);
-      setProducts((prev) => [...prev, ...newProducts]);
-    } else {
-      setIsLastPage(true);
+  /* 무한 스크롤 로직 */
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (
+        entries: IntersectionObserverEntry[],
+        observer: IntersectionObserver
+      ) => {
+        const element = entries[0];
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+          const newProducts = await getMoreProducts(page + 1);
+          if (newProducts.length !== 0) {
+            setPage((prev) => prev + 1);
+            setProducts((prev) => [...prev, ...newProducts]);
+          } else {
+            setIsLastPage(true);
+          }
+          setIsLoading(false);
+        }
+      },
+      {
+        /* 어느 정도 표시 될 때 트리거할지 여부(0 ~ 1) */
+        threshold: 1.0,
+        rootMargin: "0px 0px -100px 0px",
+      }
+    );
+    if (trigger.current) {
+      observer.observe(trigger.current);
     }
-
-    setIsLoading(false);
-  };
+    return () => {
+      observer.disconnect();
+    };
+  }, [page]);
 
   return (
     <div className="p-5 flex flex-col gap-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      {isLastPage ? (
-        <></>
-      ) : (
-        <button
-          onClick={onLoadMoreClick}
-          className="bg-red-500"
-          disabled={isLoading}
+      {!isLastPage ? (
+        <span
+          ref={trigger}
+          style={{
+            marginTop: `${page + 1 * 900}vh`,
+          }}
+          className="mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
         >
-          {isLoading ? "로딩 중" : "더 불러와"}
-        </button>
-      )}
+          {isLoading ? "로딩 중" : "Load more"}
+        </span>
+      ) : null}
     </div>
   );
 }
