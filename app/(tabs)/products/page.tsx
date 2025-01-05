4506bd30af6e75c731eb42821bcc8ee5857f6edc
umsungjun <umseongjun@naver.com>
2025-01-05 15:08:51 +0900

    Feat: 상품 목록 페이지에 상품 추가 버튼 생성

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
 import { db } from "@/lib/db";
 
 import ProductList from "@/components/product-list";
+
+import { PlusIcon } from "@heroicons/react/24/solid";
 
 const getInitialProducts = async () => {
   const products = await db.product.findMany({
@@ -26,6 +29,12 @@ export default async function Products() {
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <Link
+        href="/products/add"
+        className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-6 text-white transition-colors hover:bg-orange-400"
+      >
+        <PlusIcon className="size-10" />
+      </Link>
     </div>
   );
 }
