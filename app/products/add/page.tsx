b4b0303e18b510498aa241dd3ab4100b74d7b998
umsungjun <umseongjun@naver.com>
2025-01-05 15:09:13 +0900

    Feat: 상품 추가 페이지 마크업 완료

---
+++ app/products/add/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+
+import { PhotoIcon } from "@heroicons/react/24/solid";
+
+export default function AddProduct() {
+  const [preview, setPreview] = useState<string>("");
+  const onImageChange = (e: ChangeEvent<HTMLInputElement>) => {};
+
+  return (
+    <div>
+      <form className="flex flex-col gap-5 p-5">
+        <label
+          htmlFor="photo"
+          className="border-2 border-neutral-300 rounded-md border-dashed aspect-square flex items-center justify-center flex-col text-neutral-300 cursor-pointer"
+        >
+          <PhotoIcon className="w-20" />
+          <div className="text-neural-400 text-sm">사진을 추가해주세요.</div>
+        </label>
+        <input
+          onChange={onImageChange}
+          type="file"
+          id="photo"
+          name="photo"
+          className="hidden"
+        />
+        <Input name="title" required placeholder="제목" type="text" />
+        <Input name="price" required placeholder="가격" type="number" />
+        <Input
+          name="description"
+          required
+          placeholder="자세한 설명"
+          type="text"
+        />
+        <Button text="작성완료" />
+      </form>
+    </div>
+  );
+}
