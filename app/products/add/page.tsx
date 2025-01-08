d694b2ac66f7c8de2d203566797ac071070289ff
umsungjun <umseongjun@naver.com>
2025-01-08 21:12:13 +0900

    Feat: 업로드 된 매물 이미지 미리보기 구현

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -9,17 +9,38 @@ import { PhotoIcon } from "@heroicons/react/24/solid";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState<string>("");
-  const onImageChange = (e: ChangeEvent<HTMLInputElement>) => {};
+  const onImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+
+    if (!files) return;
+    const file = files[0];
+
+    /* 브라우저 메모리에 저장되는 이미지 주소 */
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
 
   return (
     <div>
       <form className="flex flex-col gap-5 p-5">
         <label
           htmlFor="photo"
-          className="border-2 border-neutral-300 rounded-md border-dashed aspect-square flex items-center justify-center flex-col text-neutral-300 cursor-pointer"
+          className="border-2 border-neutral-300 rounded-md border-dashed aspect-square flex items-center justify-center flex-col text-neutral-300 cursor-pointer bg-center bg-no-repeat bg-cover"
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}
         >
-          <PhotoIcon className="w-20" />
-          <div className="text-neural-400 text-sm">사진을 추가해주세요.</div>
+          {preview === "" ? (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-neural-400 text-sm">
+                사진을 추가해주세요.
+              </div>
+            </>
+          ) : (
+            /* 이미지가 업로드 된 경우 */
+            <></>
+          )}
         </label>
         <input
           onChange={onImageChange}
