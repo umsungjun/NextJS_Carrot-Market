d65fa4735e3cb9fa14a70adbe653c3e1958eb9cf
umsungjun <umseongjun@naver.com>
2025-01-09 21:47:48 +0900

    Feat: 상품 업로드 기능 구현
    - 상품 업로드를 프로젝트의 public 폴더에 내장시키는 방법으로 테스트
    - 상품 업로드 시 FormData 검증 구현

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,11 +1,75 @@
 "use server";
 
-export async function uploadProduct(formData: FormData) {
+import { z } from "zod";
+import { db } from "@/lib/db";
+/* fs fileSystem을 위한 Node.js 내장 라이브러리 */
+import fs from "fs/promises";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "사진을 등록해주세요.",
+  }),
+  title: z
+    .string({
+      required_error: "상품명을 입력해주세요.",
+    })
+    .min(1)
+    .max(30),
+  description: z.string({
+    required_error: "상품 설명을 입력해주세요.",
+  }),
+  /* z.coerce.number()는 값을 숫자로 변환하고, 변환 실패 시 에러 처리 */
+  price: z.coerce.number({
+    required_error: "가격을 입력해주세요.",
+  }),
+});
+
+export async function uploadProduct(_: any, formData: FormData) {
   const data = {
     photo: formData.get("photo"),
     title: formData.get("title"),
     price: formData.get("price"),
     description: formData.get("description"),
   };
-  console.log(data);
+
+  /* photo의 속성이 File일 때 */
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    /* 사용자가 업로드한 이미지를 프로젝트내에 public 폴더에 저장 */
+    await fs.appendFile(
+      `./public/${data.photo.name}`,
+      new Uint8Array(photoData)
+    );
+    data.photo = `/${data.photo.name}`;
+  }
+  const result = productSchema.safeParse(data);
+
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          photo: result.data.photo,
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        /* id값만 return */
+        select: {
+          id: true,
+        },
+      });
+
+      redirect(`/products/${product.id}`);
+    }
+  }
 }
